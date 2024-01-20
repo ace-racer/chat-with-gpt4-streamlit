10ce257bc9fe4cddc24511b145633da1dffd1119
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2024-01-20 15:40:04 +0800

    Add initial app

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # chat-with-gpt4-streamlit
-Chat with your GPT-4 model using OpenAI API via Streamlit web app
+Chat with your GPT-4 model using OpenAI API via Streamlit web app using latest OAI and streamlit packages
+
+# Main reference for codes:
+- https://docs.streamlit.io/knowledge-base/tutorials/build-conversational-apps
+
+## Starting the app
+
+1. Set up OpenAI API key as a streamlit secret
+Create .streamlit/secrets.toml file in our project directory and add the following lines to it:
+
+#### .streamlit/secrets.toml
+`OPENAI_API_KEY = "YOUR_API_KEY"`
+
+2. Set up virtual environment using venv in Unix/macOS:
+- `python -m venv .venv`
+- `source .venv/bin/activate`
+- `pip install -r requirements.txt`
+
+3. Start the app:
+
+1. Activate the virtual environment: `source .venv/bin/activate` (if not already activated)
+2. `streamlit run main.py`
+3. Navigate to `http://localhost:8501` where streamlit runs by default
+4. Sample run of the app: ![App image](./docs/gpt4-streamlit-localhost.png)
---
+++ main.py
@@ -0,0 +1,40 @@
+from openai import OpenAI
+import streamlit as st
+
+# Use a GPT-4 turbo model
+OAI_MODEL = "gpt-4-1106-preview"
+
+st.title("Chat with GPT-4 using Streamlit")
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = OAI_MODEL
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += (response.choices[0].delta.content or "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
